a6f56ff7bbf0a00413f17dee33e686ecb1ad0181
Yannick Panis <info@qucode.eu>
2018-04-03 16:03:34 +0200

    first commit

---
+++ FirstProgram/hello_world.py
@@ -0,0 +1,16 @@
+print("Hello World")
+print("Goodbye World")
+
+# variable rules:
+# variables start with a letter or underscore
+# rest of the name must consist of letters, numbers or underscores
+# names are case-sensitive
+
+# naming conventions:
+# snake_case
+# most should be lowercase
+# UPPERCASE_VARIABLES for constants (e.g. PI = 3.14)
+# UpperCamelCase for classes
+# variables that start and end with two underscores
+#  ("dunder" or double underscore) are private and should
+#  be left alone __no_touchy__
---
+++ MileageConverter/mileage.py
@@ -0,0 +1,4 @@
+print("How many kilometers did you run today?")
+kms = float(input())
+miles = round(kms/1.60934, 2)
+print(f"Ok, you ran {miles} miles today.")
